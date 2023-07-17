8673177c6a835f553cac0591734a47d6c6b8d129
Mustak9009 <91077142+Mustak9009@users.noreply.github.com>
2023-07-17 19:00:24 +0530

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,23 +17,10 @@ export default function Home() {
                 <h2 className="text-2xl text-gray-800 font-semibold mb-4">About Us</h2>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum, massa eu maximus euismod, velit justo eleifend libero, id aliquam dolor ex ut justo.</p>
             </section>
-
-            <section className="mt-8">
-                <h2 className="text-2xl text-gray-800 font-semibold mb-4">Services</h2>
-                <ul className="list-disc list-inside text-gray-600">
-                    <li>Service 1</li>
-                    <li>Service 2</li>
-                    <li>Service 3</li>
-                </ul>
-            </section>
         </div>
     </main>
 
-    <footer className="bg-gray-200 py-4">
-        <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>&copy; 2023 My Website. All rights reserved.</p>
-        </div>
-    </footer>
+   
     </div>
   )
 }
